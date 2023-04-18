105701c2ec1db2d1c350388220b631d96dbe1f5f
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-18 12:31:01 +0200

    add: keyup at input and newMsg function

--- milestone-3/assets/js/script.js
+++ milestone-3/assets/js/script.js
@@ -13,5 +13,18 @@ createApp({
       index: 0,
       newMsgText: ""
     }
+  },
+
+  methods: {
+    newMsg(){
+      const msg = {
+        date: dt.now().setLocale("it").toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS),
+        message: this.newMsgText,
+        status: 'sent'
+      }
+      this.contacts[this.index].messages.push(msg);
+      this.autoMsg();
+      this.newMsgText = "";
+    },
   }
 }).mount("#app")
