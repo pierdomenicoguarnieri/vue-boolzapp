01f4964a897afcce3b80387dae6ff75b2de86409
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-18 12:26:18 +0200

    add: milestone-3 folder

---
+++ milestone-3/assets/js/script.js
@@ -0,0 +1,15 @@
+import contacts from "./db.js"
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      contacts,
+      self: {
+        name: "Pierdomenico",
+        avatar: "assets/img/avatar_io.jpg"
+      },
+      index: 0,
+    }
+  }
+}).mount("#app")
