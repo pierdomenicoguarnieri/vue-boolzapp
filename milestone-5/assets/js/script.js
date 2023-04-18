f9a0ebb5b009d056605165c4813b92be7d72acb9
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-18 18:19:51 +0200

    add: milestone-5 folder

---
+++ milestone-5/assets/js/script.js
@@ -0,0 +1,50 @@
+import contacts from "./db.js"
+const {createApp} = Vue;
+const dt = luxon.DateTime;
+
+createApp({
+  data(){
+    return{
+      contacts,
+      self: {
+        name: "Pierdomenico",
+        avatar: "assets/img/avatar_io.jpg"
+      },
+      index: 0,
+      newMsgText: "",
+      checkMsg: ""
+    }
+  },
+
+  methods: {
+    newMsg(){
+      if(this.newMsgText.length > 0){
+        const msg = {
+          date: dt.now().setLocale("it").toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS),
+          message: this.newMsgText,
+          status: 'sent'
+        }
+        this.contacts[this.index].messages.push(msg);
+        this.autoMsg();
+        this.newMsgText = "";
+      }
+    },
+
+    autoMsg(){
+      setTimeout(() => {
+        const msg = {
+          date: dt.now().setLocale("it").toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS),
+          message: "Ok!",
+          status: 'received'
+        }
+        this.contacts[this.index].messages.push(msg);
+      }, 1000);
+    },
+
+    searchContact(){
+      this.contacts.forEach((contact) => {
+        contact.visible = contact.name.toLowerCase().includes(this.checkMsg.toLowerCase())
+      })
+    }
+  },
+}).mount("#app")
